33b15fb322f221325474f80e4aa42765425cba4f
gth-softdev <68547258+gth-softdev@users.noreply.github.com>
2020-10-04 16:47:40 -0400

    First run ok

---
+++ MaintenanceRecords.Data/MaintItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaintenanceRecords.Data
+{
+    public class MaintItem
+    {
+        [Key]
+        public int ItemId { get; set; }
+
+        [Required]
+        [Display(Name = "Item Name")]
+        public string ItemName { get; set; }
+        
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
+        public string MiscInfo { get; set; }
+
+        [ForeignKey(nameof(ItemLocation))]
+        public int LocationId { get; set; }
+        public virtual ItemLocation ItemLocation { get; set; }
+
+        //*[Foreign Key] OwnerId
+
+    }
+}
---
+++ MaintenanceRecords.Data/MaintRecord.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaintenanceRecords.Data
+{
+    public class MaintRecord
+    {
+        [Key]
+        public int RecordId { get; set; }
+
+        [ForeignKey(nameof(MaintItem))]
+        public int ItemId { get; set; }
+        public virtual MaintItem MaintItem { get; set; }
+
+        [Required]
+        [Display(Name = "Item notes")]
+        public string RecordText { get; set; }
+
+        [Required]
+        public DateTime RecordDate { get; set; }
+
+ 
+
+    }
+}
